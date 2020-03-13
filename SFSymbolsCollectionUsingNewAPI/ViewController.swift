163b9f949edb944955fdd139456b183061745384
stzn <sztk1209@gmail.com>
2020-03-13 10:19:46 +0900

    Initial Commit

---
+++ SFSymbolsCollectionUsingNewAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFSymbolsCollectionUsingNewAPI
+//
+//  Created by Shinzan Takata on 2020/03/13.
+//  Copyright © 2020 shiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
